837dc3ff85c245bfd65ec7137f635cb6e013bf12
Shubham PC <shubhampc@Shubhams-MacBook-Air.local>
2026-03-15 21:21:38 +0530

    added sub header

--- src/views/challenge/Fitness.jsx
+++ src/views/challenge/Fitness.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "../../assets/styles/challenge/fitness.scss";
 import Header from "../../components/Header";
+import SubHeader from "./components/SubHeader";
 const Fitness = () => {
 	return (
 		<div className="fitnessContainer">
 			<Header />
+			<SubHeader />
 		</div>
 	);
 };
---
+++ src/views/challenge/components/SubHeader.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../../../assets/styles/challenge/subHeader.scss";
+import Info from "../../../assets/svg/challenge/info.svg?react";
+import Back from "../../../assets/svg/challenge/back.svg?react";
+const SubHeader = () => {
+	return (
+		<div className="subHeaderParentContainer">
+			<div className="leftContainer">
+				<Back />
+				<span className="dayText">Back</span>
+				<span className="seperator"></span>
+				<span className="dayText">Day 1 of 9</span>
+			</div>
+			<div className="rightContainer">
+				9-Day Fitness Challenge
+				<Info />
+			</div>
+		</div>
+	);
+};
+
+export default SubHeader;
